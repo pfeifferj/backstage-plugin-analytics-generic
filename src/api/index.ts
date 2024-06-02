25eb6d96cd3d38592221f8675efd56c5465e6bfb
Josephine Pfeiffer <hi@josie.lol>
2024-06-02 10:02:28 +0200

    include session id in event

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,250 +1,259 @@
 import {
-	AnalyticsEvent,
-	ConfigApi,
-	ErrorApi,
-	IdentityApi,
-} from '@backstage/core-plugin-api';
-import { CatalogApi } from '@backstage/catalog-client';
-import { Entity } from '@backstage/catalog-model';
+  AnalyticsEvent,
+  ConfigApi,
+  ErrorApi,
+  IdentityApi,
+  SessionApi,
+  SessionState,
+} from "@backstage/core-plugin-api";
+import { CatalogApi } from "@backstage/catalog-client";
+import { Entity } from "@backstage/catalog-model";
 
 type AnalyticsAPI = {
-	captureEvent: (event: AnalyticsEvent) => void;
+  captureEvent: (event: AnalyticsEvent) => void;
 };
 
 type Options = {
-	configApi: ConfigApi;
-	errorApi: ErrorApi;
-	identityApi: IdentityApi;
-	catalogApi: CatalogApi;
+  configApi: ConfigApi;
+  errorApi: ErrorApi;
+  identityApi: IdentityApi;
+  catalogApi: CatalogApi;
+  sessionApi: SessionApi;
 };
 
 export class GenericAnalyticsAPI implements AnalyticsAPI {
-	private readonly configApi: ConfigApi;
-	private readonly catalogApi: CatalogApi;
-	private readonly errorApi: ErrorApi;
-	private readonly host: string;
-	private readonly endpoint: string;
-	private readonly identityApi: IdentityApi;
-	private eventQueue: {
-		event: AnalyticsEvent;
-		timestamp: Date;
-		user?: string;
-		teamMetadata?: Entity;
-	}[] = [];
-	private flushInterval: number;
-	private basicAuthToken?: string;
-	private retryLimit: number = 3;
-	private eventRetryCounter: Map<string, number> = new Map();
-	private debug: boolean;
+  private readonly configApi: ConfigApi;
+  private readonly catalogApi: CatalogApi;
+  private readonly errorApi: ErrorApi;
+  private readonly host: string;
+  private readonly endpoint: string;
+  private readonly identityApi: IdentityApi;
+  private readonly sessionApi: SessionApi;
+  private sessionId: string | undefined;
+  private eventQueue: {
+    event: AnalyticsEvent;
+    timestamp: Date;
+    user?: string;
+    teamMetadata?: Entity;
+    sessionId?: string;
+  }[] = [];
+  private flushInterval: number;
+  private basicAuthToken?: string;
+  private retryLimit: number = 3;
+  private eventRetryCounter: Map<string, number> = new Map();
+  private debug: boolean;
 
-	constructor(options: Options) {
-		this.configApi = options.configApi;
-		this.errorApi = options.errorApi;
-		this.catalogApi = options.catalogApi;
-		this.host = this.configApi.getString('app.analytics.generic.host');
-		this.endpoint = this.host;
-		this.identityApi = options.identityApi;
+  constructor(options: Options) {
+    this.configApi = options.configApi;
+    this.errorApi = options.errorApi;
+    this.catalogApi = options.catalogApi;
+    this.host = this.configApi.getString("app.analytics.generic.host");
+    this.endpoint = this.host;
+    this.identityApi = options.identityApi;
+    this.sessionApi = options.sessionApi;
 
-		this.debug =
-			this.configApi.getOptionalBoolean('app.analytics.generic.debug') === true;
-		const configFlushIntervalMinutes = this.configApi.getOptionalNumber(
-			'app.analytics.generic.interval'
-		);
-		this.flushInterval =
-			configFlushIntervalMinutes !== null &&
-			configFlushIntervalMinutes !== undefined
-				? configFlushIntervalMinutes * 60 * 1000
-				: 30 * 60 * 1000; // Default to 30 minutes if not specified
-		this.basicAuthToken = this.configApi.getOptionalString(
-			'app.analytics.generic.basicAuthToken'
-		);
-		if (this.flushInterval === 0) {
-			this.captureEvent = this.instantCaptureEvent;
-		} else {
-			this.startFlushCycle();
-		}
-	}
+    this.debug =
+      this.configApi.getOptionalBoolean("app.analytics.generic.debug") === true;
+    const configFlushIntervalMinutes = this.configApi.getOptionalNumber(
+      "app.analytics.generic.interval"
+    );
+    this.flushInterval =
+      configFlushIntervalMinutes !== null &&
+      configFlushIntervalMinutes !== undefined
+        ? configFlushIntervalMinutes * 60 * 1000
+        : 30 * 60 * 1000; // Default to 30 minutes if not specified
+    this.basicAuthToken = this.configApi.getOptionalString(
+      "app.analytics.generic.basicAuthToken"
+    );
 
-	static fromConfig(
-		config: ConfigApi,
-		errorApi: ErrorApi,
-		identityApi: IdentityApi,
-		catalogApi: CatalogApi
-	) {
-		return new GenericAnalyticsAPI({
-			configApi: config,
-			errorApi: errorApi,
-			identityApi,
-			catalogApi,
-		});
-	}
+    this.sessionApi.sessionState$().subscribe(this.handleSessionStateChange);
 
-	private log(message: string, isError: boolean = false): void {
-		if (this.debug) {
-			if (isError) {
-				console.error(message);
-			} else {
-				console.log(message);
-			}
-		}
-	}
+    if (this.flushInterval === 0) {
+      this.captureEvent = this.instantCaptureEvent;
+    } else {
+      this.startFlushCycle();
+    }
+  }
 
-	async captureEvent(event: AnalyticsEvent) {
-		const user = await this.getUser();
-		if (user) {
-		} else {
-			this.log('Error: user is undefined.');
-			return;
-		}
+  static fromConfig(
+    config: ConfigApi,
+    errorApi: ErrorApi,
+    identityApi: IdentityApi,
+    catalogApi: CatalogApi,
+    sessionApi: SessionApi
+  ) {
+    return new GenericAnalyticsAPI({
+      configApi: config,
+      errorApi: errorApi,
+      identityApi,
+      catalogApi,
+      sessionApi,
+    });
+  }
 
-		const teamMetadata = await this.catalogApi.getEntityByRef(user);
-		// const teamMetadata = await this.getGroup(user);
+  private handleSessionStateChange = (sessionState: SessionState) => {
+    if (sessionState === SessionState.SignedIn) {
+      this.sessionId = this.generateSessionId();
+    } else if (sessionState === SessionState.SignedOut) {
+      this.sessionId = undefined;
+    }
+  };
 
-		this.log(
-			'Capturing event: ' +
-				JSON.stringify(event) +
-				' User ID: ' +
-				user +
-				' Team Metadata: ' +
-				teamMetadata
-		);
+  private generateSessionId(): string {
+    return Math.random().toString(36).substring(2) + Date.now().toString(36);
+  }
 
-		this.eventQueue.push({
-			event,
-			timestamp: new Date(),
-			user: user,
-			teamMetadata: teamMetadata,
-		});
+  private log(message: string, isError: boolean = false): void {
+    if (this.debug) {
+      if (isError) {
+        console.error(message);
+      } else {
+        console.log(message);
+      }
+    }
+  }
 
-		if (this.flushInterval === 0) {
-			const eventToFlush = this.eventQueue.pop();
-			if (eventToFlush) {
-				this.flushEvents([eventToFlush]);
-			}
-		}
-	}
+  async captureEvent(event: AnalyticsEvent) {
+    const user = await this.getUser();
+    if (!user) {
+      this.log("Error: user is undefined.");
+      return;
+    }
 
-	private async getUser(): Promise<string | undefined> {
-		if (this.identityApi) {
-			const identity = await this.identityApi.getBackstageIdentity();
-			return identity?.userEntityRef;
-		}
-		return undefined;
-	}
+    const teamMetadata = await this.catalogApi.getEntityByRef(user);
 
-	// private async getGroup(user: string): Promise<Entity> {
-	// try {
-	// const entity: Entity = await this.catalogApi.getEntityByRef(user);
+    this.log(
+      "Capturing event: " +
+        JSON.stringify(event) +
+        " User ID: " +
+        user +
+        " Team Metadata: " +
+        teamMetadata +
+        " Session ID: " +
+        this.sessionId
+    );
 
-	// const [entityType, rest] = user.split(':');
-	// const [entityNamespace, entityName] = rest.split('/');
+    this.eventQueue.push({
+      event,
+      timestamp: new Date(),
+      user: user,
+      teamMetadata: teamMetadata,
+      sessionId: this.sessionId,
+    });
 
-	// const entity: Entity = await this.catalogApi.getEntityByName({
-	// kind: entityType,
-	// namespace: entityNamespace,
-	// name: entityName,
-	// });
+    if (this.flushInterval === 0) {
+      const eventToFlush = this.eventQueue.pop();
+      if (eventToFlush) {
+        this.flushEvents([eventToFlush]);
+      }
+    }
+  }
 
-	// return entity;
-	// } catch (error) {
-	// console.error('Failed to fetch entity ancestry:', error);
-	// }
-	// }
+  private async getUser(): Promise<string | undefined> {
+    if (this.identityApi) {
+      const identity = await this.identityApi.getBackstageIdentity();
+      return identity?.userEntityRef;
+    }
+    return undefined;
+  }
 
-	private async instantCaptureEvent(event: AnalyticsEvent) {
-		const user = await this.getUser();
-		if (!user) {
-			this.log('Error: user is undefined.');
-			return;
-		}
+  private async instantCaptureEvent(event: AnalyticsEvent) {
+    const user = await this.getUser();
+    if (!user) {
+      this.log("Error: user is undefined.");
+      return;
+    }
 
-		const teamMetadata = await this.catalogApi.getEntityByRef(user);
-		// const teamMetadata = await this.getGroup(user);
+    const teamMetadata = await this.catalogApi.getEntityByRef(user);
 
-		this.log(
-			'Capturing event: ' +
-				JSON.stringify(event) +
-				' User ID: ' +
-				user +
-				' Team Metadata: ' +
-				teamMetadata
-		);
+    this.log(
+      "Capturing event: " +
+        JSON.stringify(event) +
+        " User ID: " +
+        user +
+        " Team Metadata: " +
+        teamMetadata +
+        " Session ID: " +
+        this.sessionId
+    );
 
-		const eventWithTimestamp = {
-			event,
-			timestamp: new Date(),
-			user: user,
-			teamMetadata,
-		};
+    const eventWithTimestamp = {
+      event,
+      timestamp: new Date(),
+      user: user,
+      teamMetadata,
+      sessionId: this.sessionId,
+    };
 
-		await this.flushEvents([eventWithTimestamp]);
-	}
+    await this.flushEvents([eventWithTimestamp]);
+  }
 
-	private startFlushCycle() {
-		setInterval(async () => {
-			if (this.eventQueue.length > 0) {
-				this.flushEvents(this.eventQueue.splice(0));
-			}
-		}, this.flushInterval);
-		this.log(`Starting flush cycle with interval: ${this.flushInterval}ms`);
-	}
+  private startFlushCycle() {
+    setInterval(async () => {
+      if (this.eventQueue.length > 0) {
+        this.flushEvents(this.eventQueue.splice(0));
+      }
+    }, this.flushInterval);
+    this.log(`Starting flush cycle with interval: ${this.flushInterval}ms`);
+  }
 
-	private async flushEvents(
-		events: {
-			event: AnalyticsEvent;
-			timestamp: Date;
-			user?: string;
-			teamMetadata?: Entity;
-		}[]
-	) {
-		if (events.length === 0) {
-			this.log('No events to flush.');
-			return;
-		}
+  private async flushEvents(
+    events: {
+      event: AnalyticsEvent;
+      timestamp: Date;
+      user?: string;
+      teamMetadata?: Entity;
+      sessionId?: string;
+    }[]
+  ) {
+    if (events.length === 0) {
+      this.log("No events to flush.");
+      return;
+    }
 
-		this.log(`Flushing ${events.length} events to endpoint: ${this.endpoint}`);
+    this.log(`Flushing ${events.length} events to endpoint: ${this.endpoint}`);
 
-		try {
-			const headers: Record<string, string> = {
-				'Content-Type': 'application/json',
-			};
-			if (this.basicAuthToken) {
-				headers['Authorization'] = `Basic ${this.basicAuthToken}`;
-			}
+    try {
+      const headers: Record<string, string> = {
+        "Content-Type": "application/json",
+      };
+      if (this.basicAuthToken) {
+        headers["Authorization"] = `Basic ${this.basicAuthToken}`;
+      }
 
-			const response = await fetch(this.endpoint, {
-				method: 'POST',
-				headers: headers,
-				body: JSON.stringify(events),
-			});
+      const response = await fetch(this.endpoint, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(events),
+      });
 
-			if (!response.ok) {
-				throw new Error(
-					`Server responded with non-OK status: ${response.status}`
-				);
-			}
+      if (!response.ok) {
+        throw new Error(
+          `Server responded with non-OK status: ${response.status}`
+        );
+      }
 
-			this.log('Successfully flushed events.');
-		} catch (error) {
-			this.log('Failed to flush analytics events', true);
-			this.errorApi.post(
-				new Error(`Failed to flush analytics events: ${error}`)
-			);
+      this.log("Successfully flushed events.");
+    } catch (error) {
+      this.log("Failed to flush analytics events", true);
+      this.errorApi.post(
+        new Error(`Failed to flush analytics events: ${error}`)
+      );
 
-			events.forEach((event) => {
-				const eventId = JSON.stringify(event);
-				const retries = this.eventRetryCounter.get(eventId) || 0;
-				if (retries < this.retryLimit) {
-					this.eventQueue.push(event);
-					this.eventRetryCounter.set(eventId, retries + 1);
-					this.log(`Retrying event: ${eventId}, attempt ${retries + 1}`, true);
-				} else {
-					this.log(`Max retries reached for event: ${eventId}`, true);
-					this.errorApi.post(
-						new Error(`Max retries reached for event: ${eventId}`)
-					);
-				}
-			});
-		}
-	}
+      events.forEach((event) => {
+        const eventId = JSON.stringify(event);
+        const retries = this.eventRetryCounter.get(eventId) || 0;
+        if (retries < this.retryLimit) {
+          this.eventQueue.push(event);
+          this.eventRetryCounter.set(eventId, retries + 1);
+          this.log(`Retrying event: ${eventId}, attempt ${retries + 1}`, true);
+        } else {
+          this.log(`Max retries reached for event: ${eventId}`, true);
+          this.errorApi.post(
+            new Error(`Max retries reached for event: ${eventId}`)
+          );
+        }
+      });
+    }
+  }
 }
